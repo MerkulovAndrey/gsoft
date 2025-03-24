70e48f6363a529571296aad1d0b2280780e3830f
amerkulov <amerkulov@naukasoft.ru>
2025-03-24 14:22:00 +0300

    Данные и структура для БД

---
+++ data/01.sql
@@ -0,0 +1,48 @@
+-- менеджеры
+CREATE TABLE gsoft.managers (
+    `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'код менеджера',
+    `surname` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'фамилия менджера',
+    `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'имя менеджера',
+    `email` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'email менеджера',
+    `phone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'телефон менеджера',
+    PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci
+COMMENT='менеджеры';
+
+-- клиенты
+CREATE TABLE gsoft.clients (
+    `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'код клиента',
+    `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'название компании',
+    `inn` char(10) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'ИНН клиента',
+    `address` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'адрес клиента',
+    `email` varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'email клиента',
+    `phone` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'телефон клиента',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `clients_unique` (`inn`)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci
+COMMENT='клиенты';
+
+-- грузы
+CREATE TABLE gsoft.freights (
+    `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'код груза',
+    `container` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'номер контейнера',
+    `client_id` int unsigned NOT NULL COMMENT 'код клиента',
+    `manager_id` int unsigned NOT NULL COMMENT 'код менеджера',
+    `date_arrival` date NOT NULL COMMENT 'дата прибытия груза',
+    PRIMARY KEY (`id`),
+    KEY `freights_clients_FK` (`client_id`),
+    KEY `freights_managers_FK` (`manager_id`),
+    CONSTRAINT `freights_clients_FK` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+    CONSTRAINT `freights_managers_FK` FOREIGN KEY (`manager_id`) REFERENCES `managers` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci
+COMMENT='грузы';
+
