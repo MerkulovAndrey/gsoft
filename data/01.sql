220b8946bdb3858f8eac5fcf43d046cb195c460e
amerkulov <amerkulov@naukasoft.ru>
2025-03-25 11:26:46 +0300

    UPD: Данные и структура для БД

--- data/01.sql
+++ data/01.sql
@@ -1,10 +1,10 @@
 -- менеджеры
 CREATE TABLE gsoft.managers (
     `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'код менеджера',
-    `surname` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'фамилия менджера',
-    `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'имя менеджера',
-    `email` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'email менеджера',
-    `phone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'телефон менеджера',
+    `surname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'фамилия менджера',
+    `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'имя менеджера',
+    `email` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'email менеджера',
+    `phone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'телефон менеджера',
     PRIMARY KEY (`id`)
 )
 ENGINE=InnoDB
@@ -46,3 +46,19 @@ DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_unicode_ci
 COMMENT='грузы';
 
+-- документы груза
+CREATE TABLE gsoft.documents (
+    id INT UNSIGNED auto_increment NOT NULL COMMENT 'код документа',
+    doc_type ENUM('Счёт','Накладная', 'Санитарный') DEFAULT 'Счёт' NOT NULL COMMENT 'тип документа',
+    freight_id INT UNSIGNED NOT NULL COMMENT 'код груза',
+    doc_desc varchar(2048) NULL COMMENT 'описание документа',
+    image_id INT UNSIGNED NULL COMMENT 'ссылка на изображение документа',
+    date_create DATETIME DEFAULT now() NOT NULL COMMENT 'дата создания',
+    CONSTRAINT documents_pk PRIMARY KEY (id),
+    CONSTRAINT documents_unique UNIQUE KEY (doc_type,freight_id,image_id),
+    CONSTRAINT documents_freights_FK FOREIGN KEY (freight_id) REFERENCES gsoft.freights(id) ON DELETE CASCADE ON UPDATE CASCADE
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci
+COMMENT='документы груза';
